1cae4e6c8a43a3082f9b3e0c359f6e0088ccd497
Sebastian Quintero <sebastianquinteroz@gmail.com>
2023-02-27 06:32:34 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const dlocalInstance = dlocal('a99f78e6-5872-4690-a4bc-a0acff20c633');
+const dlocalInstance = dlocal('1ecda202-9f70-488d-8557-90d247dd5975');
 
 const today = new Date();
 const month = today.getMonth() + 1
